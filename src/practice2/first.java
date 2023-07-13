f4f6fadfc0b830b3d286dd9506f8bacee01279d0
Praveen_Gopalsamy <Praveen_Gopalsamy@EPINHYDW08DA>
2023-07-13 20:29:57 +0530

    Second Commit

--- src/practice2/first.java
+++ src/practice2/first.java
@@ -3,7 +3,9 @@ package practice2;
 public class first {
 
 	public static void main(String[] args) {
-		System.out.println("first print");
+		System.out.println("first commit");
+		
+		System.out.println("second commit");
 
 	}
 
